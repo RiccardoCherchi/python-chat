c3e626e5e7809a33ca136d1e6f542ff987dcfc95
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-06 01:22:41 +0100

    right

--- app/helpers/connection_manager.py
+++ app/helpers/connection_manager.py
@@ -1,23 +1,37 @@
+from typing import List, Dict
+
 from fastapi import WebSocket
 from app.db.config import client # redis
 
+
 class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket, user_id: str):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+	active_connections: Dict[ str, WebSocket ] = {}
+	id_to_nick: Dict[str, str] = {}
 
-        client.sadd("online_users", user_id)
+	def __init__(self):
+		pass
 
-    def disconnect(self, websocket: WebSocket, user_id: str):
-        client.srem("online_users", user_id)
-        self.active_connections.remove(websocket)
+	async def connect(self, websocket: WebSocket, user_id: str):
+		await websocket.accept()
+		self.active_connections[user_id] = websocket
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+		client.sadd("online_users", user_id)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+	def disconnect(self, websocket: WebSocket, user_id: str):
+		client.srem("online_users", user_id)
+		del self.active_connections[user_id]
+
+	async def send_personal_message(self, message: str, websocket: WebSocket):
+		await websocket.send_text(message)
+
+	async def broadcast(self, message: str):
+		for connection in self.active_connections.values():
+			await connection.send_text(message)
+
+	async def broadcastToEveryoneExcept(self, conn: WebSocket, message: str):
+		if message.startswith('{STATUS.LEFT}'):
+			message = f'{self.id_to_nick[message.replace("{STATUS.LEFT}", "", 1)]} has left the chat'
+		for connection in self.active_connections.values():
+			if connection != conn:
+				await connection.send_text(message)
--- main.py
+++ main.py
@@ -14,13 +14,13 @@ templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("chat.j2", {"request": request})
+	return templates.TemplateResponse("chat.j2", {"request": request})
 
 
 @app.get('/users', response_class=HTMLResponse)
 async def get_online_users(request: Request):
-    users = [i.decode('UTF-8') for i in client.smembers("online_users")]
-    return templates.TemplateResponse('users.j2', {"request": request, "users": users})
+	users = [i.decode('UTF-8') for i in client.smembers("online_users")]
+	return templates.TemplateResponse('users.j2', {"request": request, "users": users})
 
 
 manager = ConnectionManager()
@@ -28,16 +28,18 @@ manager = ConnectionManager()
 
 @app.websocket("/ws/{user_id}")
 async def websocket_endpoint(websocket: WebSocket, user_id: str):
-    await manager.connect(websocket, user_id)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{user_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket, user_id)
-        await manager.broadcast(f"Client #{user_id} left the chat")
+	await manager.connect(websocket, user_id)
+	try:
+		while True:
+			data = await websocket.receive_text()
+			if data.startswith('!NICK'):
+				manager.id_to_nick[user_id] = data.replace('!NICK ', '', 1)
+			await manager.send_personal_message(f"You wrote: {data}", websocket)
+			await manager.broadcastToEveryoneExcept(websocket, f'{manager.id_to_nick[user_id]}: {data}')
+	except WebSocketDisconnect:
+		manager.disconnect(websocket, user_id)
+		await manager.broadcastToEveryoneExcept(websocket, '{STATUS.LEFT}' + user_id)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", debug=True)
+	uvicorn.run("main:app", debug=True)
